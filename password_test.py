e29581c3f9059a03e8539f44eae9da9a714138dd
bera5186 <rbasu611@gmail.com>
2019-07-15 22:48:37 +0530

    Configured gitignore travis.yml file

--- password_test.py
+++ password_test.py
@@ -1,11 +1,11 @@
 import unittest
-from src import utility
+from src.utility import generate_password
 
 class PasswordTest(unittest.TestCase):
 
-    def test_for_checking_length_of_returned_password(self):
+    def test_for_checking_length_of_returned_password_list(self):
         length_list = [4,6,8]
-        list_of_passwords = utility.generate_password(length_list)
+        list_of_passwords = generate_password(length_list)
         self.assertEqual(len(list_of_passwords), 3)
 
 
